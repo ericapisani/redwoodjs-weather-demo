30e4f858a1085acbab0f7fa832a8bb9bd4748af2
Erica Pisani <pisani.erica@gmail.com>
2022-06-02 13:35:53 -0400

    Add getLocalDate edge function.
    Originally came from https://github.com/larryhudson/local-date-edge-function and modified slightly when code was converted to Typescript

---
+++ netlify/edge-functions/getLocalDate.ts
@@ -0,0 +1,30 @@
+// Taken from https://github.com/larryhudson/local-date-edge-function/blob/main/netlify/edge-functions/local-date.js
+// and modified slightly when converting the code to Typescript
+
+// import city-timezones NPM package using esm.sh CDN
+import cityTimezones from "https://esm.sh/city-timezones@v1.2.0";
+import type { Context } from "netlify:edge";
+
+function getLocalDate(utcDate: Date, geoInfo: Context["geo"]) {
+  const locationString = `${geoInfo.city} ${geoInfo.subdivision?.name} ${geoInfo.country?.name}`
+  const timezones = cityTimezones.findFromCityStateProvince(locationString);
+
+  if (timezones.length > 0) {
+    const timezoneName = timezones[0].timezone;
+    return utcDate.toLocaleString("en-US", { timeZone: timezoneName });
+  } else {
+    // no timezone found
+    return null;
+  }
+}
+
+export default async (request: Request, context: Context) => {
+  const utcDate = new Date();
+
+  const localDate = getLocalDate(utcDate, context.geo);
+
+  return context.json({
+    utcDate,
+    localDate,
+  });
+};
