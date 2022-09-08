dba2726507f1d94a6d1335ed0e46212eb3d2ef1c
David Thyresson <dthyresson@gmail.com>
2022-09-08 13:19:46 -0400

    Prettier 404

--- web/src/pages/NotFoundPage/NotFoundPage.tsx
+++ web/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,31 @@
+import { Link, routes } from '@redwoodjs/router'
+
 export default () => (
-  <main className="p-12">
-    <section>
-      <h1 className="text-red-700">
-        <p className="text-xl">Not Found</p>
-        <p className="text-lg text-red-500">Status 404</p>
-      </h1>
-    </section>
-  </main>
+  <div className="min-h-full bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+    <div className="mx-auto max-w-max">
+      <main className="sm:flex">
+        <p className="text-4xl font-bold tracking-tight text-indigo-600 sm:text-5xl">
+          404
+        </p>
+        <div className="sm:ml-6">
+          <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+              Page not found
+            </h1>
+            <p className="mt-1 text-base text-gray-500">
+              Please check the URL in the address bar and try again.
+            </p>
+          </div>
+          <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+            <Link
+              to={routes.home()}
+              className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Go back home
+            </Link>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
 )
